d4dfcfecfe28f1f712265fe2218d981202ea0ad8
Eric Berryman <eric.berryman@gmail.com>
2015-02-19 14:45:46 -0500

    Update install-docbook-xsl.sh

--- install-docbook-xsl.sh
+++ install-docbook-xsl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ ! -d "docbook-xsl-1.76.1" ]; then
-  wget "http://downloads.sourceforge.net/project/docbook/docbook-xsl/1.76.1/docbook-xsl-1.76.1.tar.bz2?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fdocbook%2Ffiles%2Fdocbook-xsl%2F1.76.1%2F&ts=1306261698&use_mirror=cdnetworks-us-2" -O docbook-xsl.tar.bz2
-  tar -xjvf docbook-xsl.tar.bz2
+  wget "http://downloads.sourceforge.net/project/docbook/docbook-xsl/1.76.1/docbook-xsl-1.76.1.tar.gz?r=&ts=1424375025&use_mirror=softlayer-dal" -O docbook-xsl.tar.gz
+  tar -xzvf docbook-xsl.tar.bz2
   rm docbook-xsl.tar.bz2
 fi
 
